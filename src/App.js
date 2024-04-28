43dfef18c0426f1f8cfd47d077bab0080c24941e
Sahan Hyanju <hyanjusahan@gmail.com>
2024-04-28 14:56:24 +0545

    slick slder css error

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ import Navbar from "./Component/Navbar/Navbar";
 import Footer from "./Component/Footer/Footer";
 import { routes } from "./Global/Datas/RoutesData";
 import ScrollToTop from "./Component/Global/ScrollToTop";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
 
 const App = () => {
   // const Homepage = lazy(() => import('./Pages/Homepage'));
